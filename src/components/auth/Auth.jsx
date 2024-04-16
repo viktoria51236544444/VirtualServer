222b31b1cb91cbc32f0f478ef23136085b2f2590
Viktoria Bratskih <peace.kokos@gmail.com>
2024-04-16 22:42:57 +0600

    start

---
+++ src/components/auth/Auth.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import style from "./auth.module.css";
+const Auth = () => {
+  return <div></div>;
+};
+
+export default Auth;
---
+++ src/routes/MainRoutes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Router, Routes } from "react-router-dom";
+import Server from "../components/home/Server";
+import Auth from "../components/auth/Auth";
+import Configuration from "../components/configuration/Configuration";
+
+const MainRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Server />} />
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/config" element={<Configuration />} />
+    </Routes>
+  );
+};
+
+export default MainRoutes;
