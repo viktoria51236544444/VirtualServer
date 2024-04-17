27eb22f5307c072ed4685e37c745f69109d9dd90
Your Name <you@example.com>
2024-04-17 17:23:15 +0600

    bayana

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Sidebar from "./components/home/Sidebar";
+import ServerTable from "./components/detail/MyServer";
+import Configuration from "./components/detail/Configuration";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Configuration />
+    </div>
+  );
 };
 
 export default App;
