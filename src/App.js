92c400f8c2fdbaabe9e21fb8bbaf8a8b40c0540e
Viktoria Bratskih <peace.kokos@gmail.com>
2024-04-18 13:59:12 +0600

    l

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import MainRoutes from "./routes/MainRoutes";
 
-import MainRoutes from "./routes/MainRoutes";
 import Sidebar from "./components/home/Sidebar";
 
 const App = () => {
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Auth from "../components/auth/Auth";
-import MyServer from "../components/detail/MyServer";
 import { Route, Routes } from "react-router-dom";
-import Order from "../components/detail/Order";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/auth" element={<Auth />} />
-      <Route path="/config" element={<Configuration />} />
     </Routes>
   );
 };
