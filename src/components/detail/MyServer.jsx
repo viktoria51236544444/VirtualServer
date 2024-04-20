48b83594da00bd6d5ef1148134713b64c3eb67e9
Your Name <you@example.com>
2024-04-20 16:35:45 +0600

    h

--- src/components/detail/MyServer.jsx
+++ src/components/detail/MyServer.jsx
@@ -7,7 +7,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { TableVirtuoso } from "react-virtuoso";
-import Box from "@mui/material/Box";
+
 import Slider from "./Slider";
 
 const localServersData = [
